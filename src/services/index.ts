92d04b1560f7d5d421e5e517ef3e376852cd84c0
Ngo Van Duc <ilovejapansong@gmail.com>
2025-12-10 17:53:14 +0700

    fix: fix index

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
 import axiosInstance from "../config/apiToken";
 import { instanceStatic } from "../config/apiStatic";
 
-export const instance = instanceStatic;
+export const instance = axiosInstance;
